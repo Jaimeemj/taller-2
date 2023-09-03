e5648a80ab5c4035ac95bf7436d8acb259b67cd3
jaime_esteban_moreno_jaramillo <je.morenoj1@uniandes.edu.co>
2023-09-03 00:23:53 -0500

    avance

--- src/uniandes/dpoo/taller2/modelo/Pedido.java
+++ src/uniandes/dpoo/taller2/modelo/Pedido.java
@@ -1,5 +1,4 @@
 package uniandes.dpoo.taller2.modelo;
-
 import java.io.File;
 import java.util.ArrayList;
 
@@ -14,31 +13,36 @@ public class Pedido extends Restaurante{
 	{
 		//	TODO hacer todo
 		this.nombreCliente = nameCliente;
-		this.direccionCliente = direcCliente;
-		
+		this.direccionCliente = direcCliente;	
 	}
-	
 	public int getPedido() {
 		return idPedido;
-		
 	}
 	public void agregarProducto(Producto nuevOItem) {
 		itemsPedido.add(nuevOItem);
 	}
 	private int getPrecioNetoPedido() {
-		//TODO hacer y cambiar value return
-		return idPedido;
+
+		int suma = 0;
+		for (int i = 0;i<itemsPedido.size();i++) {
+			Producto objProducto = itemsPedido.get(i);
+			int precio = objProducto.getPrecio();
+			suma =+ precio;
+		}
+		return suma;
 	}
 	private int getPrecioTotalPedido() {
-		//TODO hacer todo y cambiar return
-		return idPedido;
+		
+		return getPrecioIvaPedido() + getPrecioNetoPedido();
 	}
 	private int getPrecioIvaPedido() {
 		//TODO hcer todo y cambiar value return
+		
 		return idPedido;
 	}
 	private String generarTextoFactura() {
 		//TODO hacer todo y cambiar return
+		
 		return nombreCliente;
 	}
 	public void guardarFactura(File archivo) {
