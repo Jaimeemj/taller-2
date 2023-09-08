a2c8498fd6f6847a1c5fcf19f0ace8606de4b8cf
Jaimeemj <je.morenoj1@uniandes.edu.co>
2023-09-07 20:12:34 -0500

    avance aplicacion

--- src/uniandes/dpoo/taller2/consola/Aplicacion.java
+++ src/uniandes/dpoo/taller2/consola/Aplicacion.java
@@ -2,6 +2,7 @@ package uniandes.dpoo.taller2.consola;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -73,8 +74,20 @@ public class Aplicacion {
 	}
 private void ejecutarInfoPedido(int id) {
 		Pedido ped = restaurante.verifyIdPedido(id);
+		File archivo = new File("./data/"+"facturas/"+ped.getPedido()+".txt");
+		try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+			String linea = br.readLine();
+			while(linea != null) 
+			{
+				System.out.println(linea);
+				linea = br.readLine();
+;			}
 		System.out.println("SI ENTRO A EJCTINFOPEDIDO");
-		
+		}
+		catch(Exception e)
+		{e.printStackTrace();
+			
+		}
 		
 	}
 private void ejecutarCerrarPedidoYGuardarFact() { 
